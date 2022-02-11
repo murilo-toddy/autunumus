0620b5f37dfdd8c5968924ec11c0072c547b7efe
murilo-toddy <murilo.todao@usp.br>
2022-02-11 11:55:38 -0300

    Show final result

--- coneDetection.cpp
+++ coneDetection.cpp
@@ -10,14 +10,22 @@ using namespace cv;
 
 
 int main() {
-    string imagePath = "../images/1.jpg";
+    string imagePath = "../images/3.jpg";
     Mat image = imread(imagePath);
     Mat canny = getImageCanny(image, false);
 
-    vector<vector<Point>> contours = searchContours(image, canny, true);
+    vector<vector<Point>> contours = searchContours(canny, false);
 
     imshow("Original Image", image);
-    imshow("Canny Image", canny);
+//    imshow("Canny Image", canny);
+
+    Mat imageWithConeHighlight = image.clone();
+    for (int i = 0; i < contours.size(); i++) {
+        drawContours(imageWithConeHighlight, contours, i, Scalar(0, 255, 255), 2);
+    }
+
+    imshow("Cones Highlighted", imageWithConeHighlight);
+    cout << contours.size() << " cones found" << endl;
     waitKey(0);
     return 0;
 }
