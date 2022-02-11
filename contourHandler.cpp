ae178738b1a147ba61209b65815b6b220f91e939
murilo-toddy <murilo.todao@usp.br>
2022-02-11 11:34:44 -0300

    Implement pointingUp function

--- contourHandler.cpp
+++ contourHandler.cpp
@@ -9,6 +9,7 @@ vector<vector<Point>> searchContours(const Mat& originalImage, const Mat& cannyI
     Mat defaultContours(rows, cols, CV_8UC3, {0, 0, 0});
     Mat approximatedContours(rows, cols, CV_8UC3, {0, 0, 0});
     Mat convexHullContours(rows, cols, CV_8UC3, {0, 0, 0});
+    Mat desiredContours(rows, cols, CV_8UC3, {0, 0, 0});
 
     // Finds all contours in image
     findContours(cannyImage, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
@@ -35,16 +36,58 @@ vector<vector<Point>> searchContours(const Mat& originalImage, const Mat& cannyI
         if(!convexContourPointingUp(convexContours[i])) { continue; }
 
         pointingUpContours.push_back(convexContours[i]);
+        drawContours(desiredContours, pointingUpContours, pointingUpContours.size() - 1, Scalar(255, 0, 255), 2);
     }
 
     imshow("Default contours", defaultContours);
     imshow("Polygon approximation", approximatedContours);
     imshow("Convex contours", convexHullContours);
+    imshow("Pointing Upwards", desiredContours);
 
     return pointingUpContours;
 }
 
 
+
+bool compareY(Point p1, Point p2) {
+    return (p1.x < p2.x);
+}
+
+/**
+ * @brief Determines if a contour is pointing upwards
+ * @param contour
+ * @return
+ */
 bool convexContourPointingUp(vector<Point> contour) {
+    Rect boundingRectangle = boundingRect(contour);
+    double aspectRatio = boundingRectangle.width / boundingRectangle.height;
+
+    // If element's width is bigger than height, return false
+    if (aspectRatio > 0.8) { return false; }
+
+    double yCenter = boundingRectangle.tl().y + (boundingRectangle.height / 2);
+    vector<Point> pointsAboveCenter;
+    vector<Point> pointsBelowCenter;
+    for(auto& point : contour) {
+        if (point.y < yCenter) {
+            pointsAboveCenter.push_back(point);
+        } else {
+            pointsBelowCenter.push_back(point);
+        }
+    }
+
+    Mat defaultContours(960, 1280, CV_8UC3, {0, 0, 0});
+    sort(pointsBelowCenter.begin(), pointsBelowCenter.end(),
+    [](Point p1, Point p2) {
+            return (p1.x < p2.x);
+    });
+    int leftmostPointBelowCenterX = pointsBelowCenter.front().x;
+    int rightmostPointBelowCenterX = pointsBelowCenter.back().x;
+
+    for (auto& pointAbove : pointsAboveCenter) {
+        if (pointAbove.x < leftmostPointBelowCenterX || pointAbove.x > rightmostPointBelowCenterX) {
+            return false;
+        }
+    }
     return true;
 }
