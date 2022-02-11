0011ee52dbd4f7772a27ce4aca978f81758c2082
murilo-toddy <murilo.todao@usp.br>
2022-02-11 12:31:10 -0300

    Update source folder's name

--- coneDetection.cpp
+++ coneDetection.cpp
@@ -5,27 +5,31 @@
 #include "imageProcessing.h"
 #include "contourHandler.h"
 
+#define SAMPLES 13
+
 using namespace std;
 using namespace cv;
 
 
 int main() {
-    string imagePath = "../images/4.jpg";
-    Mat image = imread(imagePath);
-    Mat canny = getImageCanny(image, false);
+    for (int i = 0; i < SAMPLES; i++) {
+        string imagePath = "../source/4.jpg";
+        Mat image = imread(imagePath);
+        Mat canny = getImageCanny(image, false);
 
-    vector<vector<Point>> contours = searchContours(canny, false);
+        vector<vector<Point>> contours = searchContours(canny, false);
 
-    imshow("Original Image", image);
-    imshow("Canny Image", canny);
+        imshow("Original Image", image);
+        imshow("Canny Image", canny);
 
-    Mat imageWithConeHighlight = image.clone();
-    for (int i = 0; i < contours.size(); i++) {
-        drawContours(imageWithConeHighlight, contours, i, Scalar(0, 255, 255), 2);
+        Mat imageWithConeHighlight = image.clone();
+        for (int i = 0; i < contours.size(); i++) {
+            drawContours(imageWithConeHighlight, contours, i, Scalar(0, 255, 255), 2);
+        }
+
+        imshow("Cones Highlighted", imageWithConeHighlight);
+        cout << contours.size() << " cone(s) found" << endl;
+        waitKey(0);
     }
-
-    imshow("Cones Highlighted", imageWithConeHighlight);
-    cout << contours.size() << " cone(s) found" << endl;
-    waitKey(0);
     return 0;
 }
