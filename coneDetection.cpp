f5adb5ef52911ed8d5b016031de8fa5fd619c022
murilo-toddy <murilo.todao@usp.br>
2022-02-14 15:44:57 -0300

    Implement time analysis

--- coneDetection.cpp
+++ coneDetection.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
+#include <chrono>
 
 #include "Image.h"
 #include "imageProcessing.h"
 #include "contourHandler.h"
-#include "fileHandler.h"
-
-using namespace std;
-using namespace cv;
 
 #define STEP_BY_STEP true
 
 int main() {
+    using std::cout;
+    using std::endl;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
+
     for (int s = 1; s <= SAMPLES; s++) {
-        cout << "Processing sample " << to_string(s) << endl;
+        cout << "** Processing sample " << to_string(s) << " **" << endl;
         auto *sampledImage = new Image(s);
+        auto processBegin = high_resolution_clock::now();
 
         // Processes image to find candidate edges
         getBorderedImage(sampledImage);
+        auto processedImages = high_resolution_clock::now();
 
         // Finds all cone contours
         searchContours(sampledImage);
+        auto processedContours = high_resolution_clock::now();
+        cout << sampledImage->cont.pointingUpContours.size() << " cone(s) found" << endl;
 
+        // Save images on disk
         sampledImage->saveImagesOnDisk(STEP_BY_STEP);
+        auto processEnd = high_resolution_clock::now();
+
+        duration<double, std::milli> processImage = processedImages - processBegin;
+        duration<double, std::milli> findContours = processedContours - processedImages;
+        duration<double, std::milli> saveOnDisk   = processEnd - processedContours;
+        double coneDetection = processImage.count() + findContours.count();
+        double processTotal = coneDetection + saveOnDisk.count();
+
+        cout << "Processing image: " << processImage.count() << " ms" << endl;
+        cout << "Finding contours: " << findContours.count() << " ms" << endl;
+        cout << "Cone detection:   " << coneDetection        << " ms" << endl;
+        cout << "Saving files:     " << saveOnDisk.count()   << " ms" << endl;
+        cout << "Iteration time:   " << processTotal         << " ms" << endl;
+
+        cout << endl;
     }
     return 0;
 }
