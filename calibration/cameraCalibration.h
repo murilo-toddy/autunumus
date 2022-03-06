bbeab3b8517364cd2fd66a8a7cd6232a6ececfb0
murilo-toddy <murilo.todao@usp.br>
2022-03-06 14:45:03 -0300

    Add chessboard size as defines

--- calibration/cameraCalibration.h
+++ calibration/cameraCalibration.h
@@ -1,8 +1,19 @@
 #ifndef CONEDETECTION_CAMERACALIBRATION_H
 #define CONEDETECTION_CAMERACALIBRATION_H
 
-#include "cameraInput.h"
+#include "../cameraInput.h"
 
-void calibrateInputCamera();
+#include <iostream>
+#include <fstream>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#define SAMPLE_IMAGES_PATH "../calibration/calibration_images/*.jpg"
+#define CHESSBOARD_HEIGHT  8
+#define CHESSBOARD_WIDTH   8
+
+void calibrateCamera();
 
 #endif //CONEDETECTION_CAMERACALIBRATION_H
