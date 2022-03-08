67a011e6fcb1448380a413e18b0edae4be775f17
murilo-toddy <murilo.todao@usp.br>
2022-03-08 15:53:03 -0300

    Implement image reprojection in camera reading

--- camera/Camera.cpp
+++ camera/Camera.cpp
@@ -1,21 +1,35 @@
 #include "Camera.h"
+#include <iostream>
 
-Camera::Camera(const int& captureIndex) {
+Camera::Camera(const int &captureIndex, const int &width, const int &height) {
     this->cap.open(captureIndex);
+    this->frameSize = cv::Size(width, height);
+
+    this->intrinsicMatrix = cv::Matx33f(INTRINSIC_MATRIX_VALUES);
+    this->distortionMatrix = cv::Vec<float, 5>(DISTORTION_MATRIX_VALUES);
+
+    cv::initUndistortRectifyMap(
+            this->intrinsicMatrix, this->distortionMatrix, cv::Matx33f::eye(),
+            this->intrinsicMatrix, this->frameSize, CV_32FC1,
+            this->mapX, this->mapY
+    );
+
+    std::cout << this->mapX << "\n" << this->mapY;
 }
 
 void Camera::readFrame() {
     this->cap >> this->inputMat;
+    cv::remap(this->inputMat, this->correctedMat, this->mapX, this->mapY, cv::INTER_LINEAR);
 }
 
 cv::Mat Camera::getMat() {
-    return this->inputMat;
+    return this->correctedMat;
 }
 
 void Camera::showFrame() {
-    cv::imshow("Camera", this->inputMat);
+    cv::imshow("Camera", this->correctedMat);
 }
 
 void Camera::saveFrame(const std::string &path) {
-    cv::imwrite(path, this->inputMat);
+    cv::imwrite(path, this->correctedMat);
 }
--- camera/Camera.h
+++ camera/Camera.h
@@ -3,14 +3,25 @@
 
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+
+#include "matrices.h"
 
 class Camera {
 private:
     cv::VideoCapture cap;
-    cv::Mat inputMat;
+    cv::Mat inputMat, correctedMat;
+    cv::Mat mapX, mapY;
+    cv::Size frameSize;
+    cv::Matx33f intrinsicMatrix;
+    cv::Vec<float, 5> distortionMatrix;
 
 public:
-    Camera(const int& captureIndex);
+    explicit Camera(
+        const int& captureIndex = 0,
+        const int& width        = 1280,
+        const int& height       = 960
+    );
 
     cv::Mat getMat();
     void readFrame();
