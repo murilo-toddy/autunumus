b41d54f56b4c9f8e149f4b26fd98ad0502c6b392
murilo-toddy <murilo.todao@usp.br>
2022-03-08 15:33:31 -0300

    Implement default camera reading class

--- camera/Camera.cpp
+++ camera/Camera.cpp
@@ -1,5 +1,21 @@
-//
-// Created by todao on 3/5/22.
-//
-
 #include "Camera.h"
+
+Camera::Camera(const int& captureIndex) {
+    this->cap.open(captureIndex);
+}
+
+void Camera::readFrame() {
+    this->cap >> this->inputMat;
+}
+
+cv::Mat Camera::getMat() {
+    return this->inputMat;
+}
+
+void Camera::showFrame() {
+    cv::imshow("Camera", this->inputMat);
+}
+
+void Camera::saveFrame(const std::string &path) {
+    cv::imwrite(path, this->inputMat);
+}
--- camera/Camera.h
+++ camera/Camera.h
@@ -1,13 +1,21 @@
-//
-// Created by todao on 3/5/22.
-//
-
 #ifndef PATHDETECTIONALGORITHM_CAMERA_H
 #define PATHDETECTIONALGORITHM_CAMERA_H
 
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 class Camera {
+private:
+    cv::VideoCapture cap;
+    cv::Mat inputMat;
 
+public:
+    Camera(const int& captureIndex);
+
+    cv::Mat getMat();
+    void readFrame();
+    void showFrame();
+    void saveFrame(const std::string& path);
 };
 
 
