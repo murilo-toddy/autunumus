b6019f500d9113a9f96efab8cc9cada986a10f8f
murilo-toddy <murilo.todao@usp.br>
2022-03-12 00:58:36 -0300

    Remove unused variables

--- processing/imageProcessing.h
+++ processing/imageProcessing.h
@@ -10,17 +10,6 @@
 
 #include "../frame/Image.h"
 
-
-// Colors in (H, S, V) format to be considered
-//#define LOW_RED
-//#define HIGH_RED
-
-//#define HSV_LOW_BOTTOM  {0, 135, 135}
-//#define HSV_LOW_TOP     {15, 255, 255}
-//#define HSV_UP_BOTTOM   {159, 135, 135}
-//#define HSV_UP_TOP      {179, 255, 255}
-//std::map<std::string, std::vector<int>> colorMapping;
-
 // Canny edges threshold
 #define CANNY_LOW       80.0
 #define CANNY_HIGH      160.0
