54b13fc0c2cb5aba44d7a4a25f367804e0fa6510
murilo-toddy <murilo.todao@usp.br>
2022-03-13 12:34:14 -0300

    Remove cv namespace from sample image

--- setup/colorFiltering.cpp
+++ setup/colorFiltering.cpp
@@ -4,35 +4,29 @@ int hmin = 0, smin = 0, vmin = 0;
 int hmax = 179, smax = 255, vmax = 255;
 
 void createTrackbar() {
-    using namespace cv;
-    namedWindow("Trackbar", WINDOW_AUTOSIZE);
-    createTrackbar("Hue Min", "Trackbar", &hmin, 179);
-    createTrackbar("Hue Max", "Trackbar", &hmax, 179);
-    createTrackbar("Sat Min", "Trackbar", &smin, 255);
-    createTrackbar("Sat Max", "Trackbar", &smax, 255);
-    createTrackbar("Val Min", "Trackbar", &vmin, 255);
-    createTrackbar("Val Max", "Trackbar", &vmax, 255);
+    cv::namedWindow("Trackbar", cv::WINDOW_AUTOSIZE);
+    cv::createTrackbar("Hue Min", "Trackbar", &hmin, 179);
+    cv::createTrackbar("Hue Max", "Trackbar", &hmax, 179);
+    cv::createTrackbar("Hue Max", "Trackbar", &hmax, 179);
+    cv::createTrackbar("Sat Min", "Trackbar", &smin, 255);
+    cv::createTrackbar("Val Min", "Trackbar", &vmin, 255);
+    cv::createTrackbar("Val Max", "Trackbar", &vmax, 255);
 }
 
-void showImage(const std::string &imagePath) {
-    using namespace cv;
+void findColorSpectrumSampleImage(const std::string &imagePath) {
     createTrackbar();
-
-    Mat image = imread(imagePath);
-    Mat hsv, mask;
+    cv::Mat image = cv::imread(imagePath), hsv, mask;
 
     while(true) {
-        imshow("Image", image);
+        cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV);
+        cv::Scalar lower(hmin, smin, vmin);
+        cv::Scalar upper(hmax, smax, vmax);
+        cv::inRange(hsv, lower, upper, mask);
 
-        cvtColor(image, hsv, COLOR_BGR2HSV);
-        Scalar lower(hmin, smin, vmin);
-        Scalar upper(hmax, smax, vmax);
-        inRange(hsv, lower, upper, mask);
+        cv::imshow("Original image", image);
+        cv::imshow("Masked image", mask);
 
-        imshow("HSV", hsv);
-        imshow("Mask", mask);
-
-        waitKey(1);
+        cv::waitKey(1);
     }
 }
 
