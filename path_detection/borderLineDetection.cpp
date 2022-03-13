64bda7593dbe446fba53d1984c4d9c9764fbed7b
murilo-toddy <murilo.todao@usp.br>
2022-03-13 16:29:03 -0300

    Implement color filtering in find road marking functions

--- path_detection/borderLineDetection.cpp
+++ path_detection/borderLineDetection.cpp
@@ -1,1 +1,46 @@
 #include "borderLineDetection.h"
+
+std::map<std::string, std::vector<cv::Scalar>> loadColorMapRoadMarkings() {
+    // Scalars are in (H, S, V) format
+    // Each color contains a lower and upper end
+    return {
+        { "WHITE",  {{0, 0, 207}, {110, 22, 255}}},
+        { "YELLOW", {}}
+    };
+}
+
+void findRoadMarkings() {
+    /*
+     * TODO
+     * Change image format to birds eye view
+     * Filter for road markings
+     * Delimit them and save their information in a structure
+     */
+
+    // Collect all sample files
+    std::vector<cv::String> files;
+    cv::glob(BORDER_IMAGES_PATH, files, false);
+    std::map<std::string, std::vector<cv::Scalar>> colorMap = loadColorMapRoadMarkings();
+
+    for (auto const& file : files) {
+        std::cout << "** Processing sample " << std::string(file) << " **\n";
+        cv::Mat image = cv::imread(file), hsv;
+
+        cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV);
+        int index = 0;
+        std::vector<cv::Mat> rangedImages(colorMap.size());
+        // Filter for specific image colors
+        for (auto const& [key, value] : colorMap) {
+            cv::inRange(hsv, value[0], value[1], rangedImages[index]);
+            index++;
+        }
+
+        cv::Mat maskedImage = rangedImages[0].clone();
+        for (int i = 1; i < (int)colorMap.size(); i++) {
+            cv::addWeighted(maskedImage, 1, rangedImages[i], 1, 0, maskedImage);
+        }
+
+        cv::imshow("Image", image);
+        cv::waitKey(0);
+    }
+}
--- path_detection/borderLineDetection.h
+++ path_detection/borderLineDetection.h
@@ -1,4 +1,16 @@
 #ifndef CONEDETECTION_BORDERLINEDETECTION_H
 #define CONEDETECTION_BORDERLINEDETECTION_H
 
+#include <iostream>
+#include <vector>
+#include <map>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#define BORDER_IMAGES_PATH "../path_detection/test_images"
+
+void findRoadMarkings();
+
 #endif //CONEDETECTION_BORDERLINEDETECTION_H
