9adf41ea3317f042ba1ac9aef87897e90b266a2f
murilo-toddy <murilo.todao@usp.br>
2022-03-26 18:04:42 -0300

    Update mode

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ enum OPERATION_MODE {
 };
 
 int main(int, char**) {
-    OPERATION_MODE operation = PATH_DETECTION;
+    OPERATION_MODE operation = CONE_DETECTION;
     switch (operation) {
         case COLOR_PICKING:
              findColorSpectrumSampleImage("../path_detection/test_images/1.jpg");
@@ -23,8 +23,8 @@ int main(int, char**) {
             break;
 
         case CONE_DETECTION:
-            coneDetectionSampledImages();
-//            coneDetectionVideo();
+//            coneDetectionSampledImages();
+            coneDetectionVideo();
             break;
 
         case PATH_DETECTION:
