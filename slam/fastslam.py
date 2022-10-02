cfdb6aafe5904e55d978f36362f5427c1986fba0
murilo_toddy <murilo.todao@usp.br>
2022-10-02 17:38:00 -0300

    Implement FastSLAM class

---
+++ slam/fastslam.py
@@ -0,0 +1,87 @@
+from math import sqrt
+import random
+import numpy as np
+import copy
+
+class FastSLAM:
+    def __init__(self, initial_particles,
+                 robot_width, scanner_displacement,
+                 control_motion_factor, control_turn_factor,
+                 measurement_distance_stddev, measurement_angle_stddev,
+                 minimum_correspondence_likelihood):
+        # The particles.
+        self.particles = initial_particles
+
+        # Some constants.
+        self.robot_width = robot_width
+        self.scanner_displacement = scanner_displacement
+        self.control_motion_factor = control_motion_factor
+        self.control_turn_factor = control_turn_factor
+        self.measurement_distance_stddev = measurement_distance_stddev
+        self.measurement_angle_stddev = measurement_angle_stddev
+        self.minimum_correspondence_likelihood = \
+            minimum_correspondence_likelihood
+
+    def predict(self, control):
+        """The prediction step of the particle filter."""
+        left, right = control
+        left_std  = sqrt((self.control_motion_factor * left)**2 +\
+                        (self.control_turn_factor * (left-right))**2)
+        right_std = sqrt((self.control_motion_factor * right)**2 +\
+                         (self.control_turn_factor * (left-right))**2)
+        # Modify list of particles: for each particle, predict its new position.
+        for p in self.particles:
+            l = random.gauss(left, left_std)
+            r = random.gauss(right, right_std)
+            p.move(l, r)
+
+    def update_and_compute_weights(self, cylinders):
+        """Updates all particles and returns a list of their weights."""
+        Qt_measurement_covariance = \
+            np.diag([self.measurement_distance_stddev**2,
+                     self.measurement_angle_stddev**2])
+        weights = []
+        for p in self.particles:
+            # Added: decrement landmark counter for any landmark that should be
+            # visible.
+            p.decrement_visible_landmark_counters(self.scanner_displacement)
+
+            # Loop over all measurements.
+            number_of_landmarks = p.number_of_landmarks()
+            weight = 1.0
+            for measurement, measurement_in_scanner_system in cylinders:
+                weight *= p.update_particle(
+                    measurement, measurement_in_scanner_system,
+                    number_of_landmarks,
+                    self.minimum_correspondence_likelihood,
+                    Qt_measurement_covariance, self.scanner_displacement)
+
+            # Append overall weight of this particle to weight list.
+            weights.append(weight)
+            
+            # Added: remove spurious landmarks (with negative counter).
+            p.remove_spurious_landmarks()
+
+        return weights
+
+    def resample(self, weights):
+        """Return a list of particles which have been resampled, proportional
+           to the given weights."""
+        new_particles = []
+        max_weight = max(weights)
+        index = random.randint(0, len(self.particles) - 1)
+        offset = 0.0
+        for i in range(len(self.particles)):
+            offset += random.uniform(0, 2.0 * max_weight)
+            while offset > weights[index]:
+                offset -= weights[index]
+                index = (index + 1) % len(weights)
+            new_particles.append(copy.deepcopy(self.particles[index]))
+        return new_particles
+
+    def correct(self, cylinders):
+        """The correction step of FastSLAM."""
+        # Update all particles and compute their weights.
+        weights = self.update_and_compute_weights(cylinders)
+        # Then resample, based on the weight array.
+        self.particles = self.resample(weights)
