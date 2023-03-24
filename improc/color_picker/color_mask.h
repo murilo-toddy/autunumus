a9dcda9e07d2a4dc3950fe729474684f31dd137f
murilo-toddy <murilo.todao@gmail.com>
2023-03-24 10:15:35 -0300

    Update image path

--- improc/color_picker/color_mask.h
+++ improc/color_picker/color_mask.h
@@ -5,7 +5,7 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 
-#define SOURCE_IMAGE_PATH "./cone_detection/source_images/1.jpg"
+#define SOURCE_IMAGE_PATH "./images/cone_sample/1.jpg"
 #define CAMERA_INDEX 0
 
 enum mode {
