326dc5cc1ceefa89c006a52f088925aecd8548e9
murilo-toddy <murilo.todao@gmail.com>
2023-03-24 09:38:09 -0300

    Remove color picking methods from main

--- improc/main.cpp
+++ improc/main.cpp
@@ -1,12 +1,10 @@
-#include "setup/colorFiltering.h"
 #include "cone_detection/coneDetection.h"
 #include "path_detection/borderLineDetection.h"
-// #include "setup/cameraCalibration.h"
 #define PATH "../path_detection/test_images/1.jpg"
+// #include "setup/cameraCalibration.h"
 
 enum OPERATION_MODE {
     COLOR_PICKING,
-    CAMERA_CALIBRATION,
     CONE_DETECTION,
     PATH_DETECTION
 };
@@ -21,18 +19,6 @@ INPUT_MODE inputMode = CAMERA_INPUT;
 
 int main(int, char**) {
     switch (operation) {
-        case COLOR_PICKING:
-            if (inputMode == SAMPLED_IMAGE) {
-                findColorSpectrumSampleImage(PATH);
-            } else {
-                findColorSpectrumVideo();
-            }
-            break;
-        
-        case CAMERA_CALIBRATION:
-            // calibrateCamera();
-            break;
-
         case CONE_DETECTION:
             if (inputMode == SAMPLED_IMAGE) {
                 coneDetectionSampledImages();
