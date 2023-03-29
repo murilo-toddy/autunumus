676219fb61688295e5f5304641f71b2a6cc5a4b1
murilo_toddy <murilo.todao@usp.br>
2023-03-29 16:58:18 -0300

    Implement sampled images option

--- improc/main.cpp
+++ improc/main.cpp
@@ -7,7 +7,7 @@
 #include "cone_detection/cone_detection.h"
 #include "path_detection/roadmark_detection.h"
 
-#define SAMPLE_IMAGES_PATH "images/samples/*"
+#define SAMPLE_IMAGES_PATH "images/cone_sample/*"
 
 enum IMAGE_INPUT_MODE {
     SAMPLE_IMAGES,
@@ -15,8 +15,7 @@ enum IMAGE_INPUT_MODE {
 };
 
 
-const IMAGE_INPUT_MODE operation_mode = CAMERA_INPUT;
-
+const IMAGE_INPUT_MODE operation_mode = SAMPLE_IMAGES;
 
 int main(int, char**) {
     cv::Mat image;
@@ -24,7 +23,28 @@ int main(int, char**) {
     if(operation_mode == SAMPLE_IMAGES) {
         auto files = file::load_images_from_query(SAMPLE_IMAGES_PATH);
         for(int i = 0; i < files.size(); i++) {
-            // TODO implement this
+            auto cycle_begin = std::chrono::high_resolution_clock::now();
+
+            image = cv::imread(files[i]);
+
+            auto collected_frame = std::chrono::high_resolution_clock::now();
+            std::cout << "updating frame took " << std::chrono::duration_cast<
+                std::chrono::milliseconds>(collected_frame
+                        - cycle_begin).count() << "\n";
+
+            auto cones = find_cones(image);
+
+            auto processed_cones = std::chrono::high_resolution_clock::now();
+            std::cout << "updating cones took " << std::chrono::duration_cast<
+                std::chrono::milliseconds>(processed_cones - collected_frame
+                ).count() << "\n";
+            
+            cv::imshow("camera", cones.images.back().second);
+            cv::waitKey(0);
+
+            auto cycle_end = std::chrono::high_resolution_clock::now();
+            std::cout << "fps " << 1000 / std::chrono::duration_cast<
+                std::chrono::milliseconds>(cycle_end - cycle_begin).count() << "\n";
         }
     }
 
@@ -44,8 +64,8 @@ int main(int, char**) {
 
             auto processed_cones = std::chrono::high_resolution_clock::now();
             std::cout << "updating cones took " << std::chrono::duration_cast<
-                std::chrono::milliseconds>(processed_cones
-                        - collected_frame).count() << "\n";
+                std::chrono::milliseconds>(processed_cones - collected_frame
+                ).count() << "\n";
             
             cv::imshow("camera", cones.images.back().second);
             cv::waitKey(1);
@@ -56,4 +76,3 @@ int main(int, char**) {
         }
     }
 }
-
