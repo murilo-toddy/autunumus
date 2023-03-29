32a81d16485f33da20679d5abcd96cc2614c2e2a
murilo-toddy <murilo.todao@gmail.com>
2023-03-29 15:41:35 -0300

    Add threads support

--- improc/main.cpp
+++ improc/main.cpp
@@ -1,10 +1,11 @@
-#include "config.h"
-#include "cone_detection/cone_detection.h"
-#include "path_detection/roadmark_detection.h"
-#include "camera/Camera.h"
-#include "io/file_handler.h"
 #include <opencv2/highgui.hpp>
 #include <chrono>
+
+#include "config.h"
+#include "io/file_handler.h"
+#include "camera/Camera.h"
+#include "cone_detection/cone_detection.h"
+#include "path_detection/roadmark_detection.h"
 
 #define SAMPLE_IMAGES_PATH "images/samples/*"
 
@@ -23,7 +24,7 @@ int main(int, char**) {
     if(operation_mode == SAMPLE_IMAGES) {
         auto files = file::load_images_from_query(SAMPLE_IMAGES_PATH);
         for(int i = 0; i < files.size(); i++) {
-
+            // TODO implement this
         }
     }
 
@@ -31,8 +32,20 @@ int main(int, char**) {
         Camera camera;
         while(camera.is_open()) {
             auto cycle_begin = std::chrono::high_resolution_clock::now();
+
             image = camera.update_frame();
+
+            auto collected_frame = std::chrono::high_resolution_clock::now();
+            std::cout << "updating frame took " << std::chrono::duration_cast<
+                std::chrono::milliseconds>(collected_frame
+                        - cycle_begin).count() << "\n";
+
             auto cones = find_cones(image);
+
+            auto processed_cones = std::chrono::high_resolution_clock::now();
+            std::cout << "updating cones took " << std::chrono::duration_cast<
+                std::chrono::milliseconds>(processed_cones
+                        - collected_frame).count() << "\n";
             
             cv::imshow("camera", cones.images.back().second);
             cv::waitKey(1);
