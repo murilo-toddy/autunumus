e05e4342890c79120816f2aacb2ea6ea15bcd538
murilo-toddy <murilo.todao@gmail.com>
2023-08-11 09:25:28 -0300

    implement zmq provider

---
+++ slam/zmqprovider.py
@@ -0,0 +1,32 @@
+import zmq
+import numpy as np
+
+
+class ZMQProvider:
+    def __init__(self):
+        self.context = zmq.Context()
+        base_address = "tcp://127.0.0.1"
+
+        vehicle_address = f"{base_address}:5555"
+        scanner_address = f"{base_address}:5556"
+
+        self.vehicle_socket = self.__setup_pull_socket(vehicle_address)
+        self.scanner_socket = self.__setup_pull_socket(scanner_address)
+
+    def __setup_pull_socket(self, address: str):
+        socket = self.context.socket(zmq.PULL)
+        socket.connect(address)
+        return socket
+
+    def __socket_pop(self, socket: zmq.Socket):
+        return socket.recv_pyobj()
+
+    def read_vehicle_data(self) -> list[float]:
+        return self.__socket_pop(self.vehicle_socket)
+
+    def read_scanner_data(self) -> np.ndarray:
+        return self.__socket_pop(self.scanner_socket)
+
+    def read_data(self) -> tuple[list[float], np.ndarray]:
+        return self.read_vehicle_data(), self.read_scanner_data()
+
