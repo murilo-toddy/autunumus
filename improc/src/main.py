c95192a92c18910cb021edb7ff53f96818cb4fc1
murilo_toddy <murilo.todao@gmail.com>
2023-09-26 10:51:22 -0300

    refactor: bump modules organization

---
+++ improc/src/main.py
@@ -0,0 +1,39 @@
+import logging
+import time
+import zmq
+import cv2
+
+
+logging.basicConfig(
+        level=logging.INFO, 
+        datefmt="%d-%m-%y %H:%M:%S",
+        format="[%(asctime)s - %(levelname)s] %(module)s: %(message)s", 
+)
+
+logger = logging.getLogger()
+
+
+if __name__ == "__main__":
+    context = zmq.Context()
+
+    base_address = "tcp://localhost"
+    frame_port = f"{base_address}:5555"
+
+    frame_socket = context.socket(zmq.PULL)
+    frame_socket.connect(frame_port)
+
+    while True:
+        start_time = time.time()
+        frame = frame_socket.recv_pyobj()
+        cv2.imshow("Input", frame)
+        
+        c = cv2.waitKey(1)
+        if c == 27:
+            logger.info("Escape key pressed, stopping")
+            frame_socket.close()
+            quit()
+        
+        end_time = time.time()
+        logger.info(f"Frame acquisition took {round(end_time - start_time, 4):<6}")
+
+
